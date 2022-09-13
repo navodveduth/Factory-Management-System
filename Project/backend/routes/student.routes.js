53db4f8e1e72ac21cb0ed6321fe242cb1d7ac759
IT21003028 <it21003028@my.sliit.lk>
2022-09-13 14:53:56 +0530

    added routes

--- Project/backend/routes/student.routes.js
+++ Project/backend/routes/student.routes.js
@@ -1,9 +1,12 @@
-import {getAllStudents, addStudent} from "../controllers/student.controller.js";
+import {getAllStudents, getOneStudent, addStudent, updateStudent, deleteStudent} from "../controllers/student.controller.js";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/", getAllStudents); 
+router.get("/:id", getOneStudent);
 router.post("/create", addStudent); 
+router.delete("/delete/:id", deleteStudent);
+router.put("/update/:id", updateStudent)
 
 export default router;
