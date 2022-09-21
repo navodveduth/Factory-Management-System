7ac90ba69cc1ccb92325a06b5f651aa33d45ffd2
it21004322 <it21004322@my.sliit.lk>
2022-09-21 13:02:44 +0530

    maintainence routes implemented

---
+++ Project/backend/routes/maintainence.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getAllMaintainenceDetails, getOneMaintainenceDetail,createMaintainenceDetails, updateMaintainenceDetails, deleteMaintainenceDetails} from "../controllers/maintainence.controller.js";
+
+const router = express.Router();
+
+//http://localhost:80700/
+router.get("/", getAllMaintainenceDetails);
+router.get("/:id", getOneMaintainenceDetail);
+router.post("/create", createMaintainenceDetails);
+router.put("/update/:id",updateMaintainenceDetails);
+router.delete("/delete/:id",deleteMaintainenceDetails);
+
+export default router;
