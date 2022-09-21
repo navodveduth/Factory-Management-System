57f02e160822b93f0cffab51f797799119a585e8
it21004322 <it21004322@my.sliit.lk>
2022-09-21 13:01:09 +0530

    machinery controller implemented

---
+++ Project/backend/controllers/machinery.controller.js
@@ -0,0 +1,62 @@
+import Machinery from "../models/machinery.model.js";
+
+export const getAllMachineryDetails = async (req, res) => {
+    try {
+        const machinery = await Machinery.find();
+        res.status(200).json(machinery);    
+    } catch (error) {
+        res.status(404).json({ message : error});
+    }
+}
+
+export const getOneMachineryDetail = async (req, res) =>{
+    try {
+        const id= req.params.id;
+       const machineries= await Machinery.findById(id);
+        res.status(200).json(machineries);
+    } catch (error) {
+        res.status(404).json({ message : error});
+    }
+
+}
+
+export const createMachineryDetails = async (req, res) =>{
+    try {
+        const machinery = req.body;
+        const newMachinery = new Machinery(machinery);
+        await newMachinery.save();
+        res.status(200).json(newMachinery);
+    } catch (error) {
+        res.status(404).json({ message : error});
+    }
+
+}
+
+export const updateMachineryDetails = async (req, res) =>{
+    try {
+        const id= req.params.id;
+        const machinery = req.body;
+        await Machinery.findByIdAndUpdate(id, machinery);
+        res.status(200).json({
+            status : "Machinery details updated"
+        });
+
+    } catch (error) {
+        res.status(404).json({ message : error});
+    }
+
+}
+
+export const deleteMachineryDetails = async (req, res) =>{
+    try {
+        const id = req.params.id;
+        await Machinery.findByIdAndDelete(id);
+        res.status(200).json({
+        status : "Machinery details deleted"
+    })
+        
+    } catch (error) {
+        res.status(404).json({ message : error});
+    }
+
+}
