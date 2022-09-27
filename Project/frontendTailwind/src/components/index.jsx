73b952fd9d22ba916418f594a52195cc2ffecc61
IT20159030 <it20159030@my.sliit.lk>
2022-09-28 00:27:49 +0530

    created the frontendTailwind - sachini

---
+++ Project/frontendTailwind/src/components/index.jsx
@@ -0,0 +1,12 @@
+export { default as Button } from './Button';
+export { default as ThemeSettings } from './ThemeSettings';
+export { default as Sidebar } from './Sidebar';
+// eslint-disable-next-line import/no-cycle
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as UserProfile } from './UserProfile';
+export { default as ChartsHeader } from './ChartsHeader';
+export { default as Header } from './Header';
+export { default as DashTopBox } from './DashTopBox';
+export { default as DashTopButton } from './DashTopButton';
+
