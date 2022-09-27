49d25d5fe02d580133cf2395aab2a6746da95569
Minod Perera <87423225+IT21003028@users.noreply.github.com>
2022-09-27 15:00:05 +0530

    Revert "Revert "Merge pull request #44 from SLIITITP/dev/devindu""
    This reverts commit c1e4de17607031b46f70c2335c246ed321c0628d.

--- Project/backend/models/machinery.model.js
+++ Project/backend/models/machinery.model.js
@@ -3,26 +3,75 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const machinerySchema = new Schema({
-    name : {
-        type : String, 
-        required : true
-    },
-    dateOfPurchased : {
-        type : Date,
-        required : true
-    },
-    depreciation : {
-        type : String,
-        required : true
-    },
-    machineryCost:{
-       type: Number,
-       required: true
-    },
-    others:{
+    machineID: {
+
         type: String,
+
+        required: true,
+
+        unique: true,
+
+        trim: true,
+
+        minlength: 1,
+
+        maxLength: 20,    
+
+       
+
+    },
+
+    name : {
+
+        type : String,
+
+        required : true
+
+    },
+
+    dateOfPurchased : {
+
+        type : Date,
+
+        required : true
+
+    },
+
+
+
+    machineryCost:{
+
+        type: Number,
+
+        required: true
+
+     },
+
+   
+
+     salvage:{
+
+         type: Number,
+
+         required:true
+
+     },
+
+     numberOfYrs:{
+
+         type: Number,
+
+         required:true
+
+     },
+
+    others:{
+
+        type: String,
+
         required:true
-    }
+
+    },
 });
 
 const Machinery = mongoose.model("machinery" , machinerySchema);
---
+++ Project/frontend/src/Components/Production/ProdHeader.js
@@ -0,0 +1,51 @@
+import React from "react";
+import {Link, link} from "react-router-dom";
+
+function ProdHeader(){
+
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#" style={{color: "#ff5f5a",fontFamily:"sans-serif"}}>Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+      <li>
+          <Link to="/" className="nav-link">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/production/order/addOrder" aria-current="page" className="nav-link active">Create an Order</Link>
+        </li>
+        <li className="nav-item">
+         <Link to="/production/order/allOrders" aria-current="page" className="nav-link active">Check Orders</Link>
+        </li>
+
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    
+    );
+}
+
+export default ProdHeader;
