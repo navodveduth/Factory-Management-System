63329ed4206f7eb617dac24c5261f35abae7d9dd
Navod <54998118+navodveduth@users.noreply.github.com>
2022-09-28 13:51:00 +0530

    Created responsive table

---
+++ Project/frontendTailwind/src/components/Table/TableData.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TableData = ({ value }) => (
+  <td className="text-center px-3 align-middle border-l-0 border-r-0 text-m whitespace-nowrap p-3">
+    {value}
+  </td>
+);
+
+export default TableData;
---
+++ Project/frontendTailwind/src/components/Table/TableHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TableHeader = ({ value }) => (
+  <td className="px-4 py-3 text-md whitespace-nowrap font-semibold text-center text-black-300">
+    {value}
+  </td>
+);
+
+export default TableHeader;
