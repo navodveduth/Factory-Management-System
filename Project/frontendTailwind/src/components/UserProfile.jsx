1ad9f5ce10ba21270326b36ceee8bf00844a2f84
IT20159030 <it20159030@my.sliit.lk>
2022-09-28 23:25:22 +0530

    Did changes to frontend

--- Project/frontendTailwind/src/components/UserProfile.jsx
+++ Project/frontendTailwind/src/components/UserProfile.jsx
@@ -33,24 +33,7 @@ const UserProfile = () => {
           <p className="text-gray-500 text-sm font-semibold dark:text-gray-400"> info@shop.com </p>
         </div>
       </div>
-      <div>
-        {userProfileData.map((item, index) => (
-          <div key={index} className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]">
-            <button
-              type="button"
-              style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-              className=" text-xl rounded-lg p-3 hover:bg-light-gray"
-            >
-              {item.icon}
-            </button>
-
-            <div>
-              <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
-              <p className="text-gray-500 text-sm dark:text-gray-400"> {item.desc} </p>
-            </div>
-          </div>
-        ))}
-      </div>
+      
       <div className="mt-5">
         <Button
           color="white"
--- Project/frontendTailwind/src/pages/AttendanceViewAll.jsx
+++ Project/frontendTailwind/src/pages/AttendanceViewAll.jsx
@@ -1,8 +1,95 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { Header } from '../components';
+import { useStateContext } from '../contexts/ContextProvider';
+import TableData from '../components/Table/TableData';
+import TableHeader from '../components/Table/TableHeader';
 
 const AttendanceViewAll = () => {
+  const { currentColor } = useStateContext();
+
+  const [attendance, setAttendance] = useState([]);
+
+  const getAttendance = async () => {
+    axios
+      .get('http://localhost:8070/attendance/viewAttendance')
+      .then((res) => {
+        setAttendance(res.data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
+  useEffect(() => {
+    getAttendance();
+  }, []);
+
+  const deleteAttendance = async (id) => {
+    await axios
+      .delete(`http://localhost:8070/attendance/deleteAttendance/${id}`)
+      .then((res) => {
+        alert('Data deleted successfully');
+        getAttendance();
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
   return (
-    <div>AttendanceViewAll</div>
+    <div>
+      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg dark:text-white">
+        <Header category="Table" title="Attendance" />
+        <div className="block w-full overflow-x-auto rounded-lg">
+          <table className="w-full rounded-lg">
+            <thead>
+              <tr className="bg-slate-200 text-md h-12 dark:bg-slate-800">
+                <TableHeader value="Employee ID" />
+                <TableHeader value="Date" />
+                <TableHeader value="In-Time" />
+                <TableHeader value="Out-Time" />
+                <TableHeader value="Status" />
+                <TableHeader value="Manage" />
+              </tr>
+            </thead>
+            <tbody>
+              {attendance.map((data) => (
+                <tr className="text-sm h-10 border dark:border-slate-600">
+                  <TableData value={data.employeeNumber} />
+                  <TableData value={new Date(data.attendanceDate).toISOString().split('T')[0]} />
+                  <TableData value={new Date(data.employeeInTime).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })} />
+                  <TableData value={new Date(data.employeeOutTime).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })} />
+                  <TableData value={data.attendanceStatus} />
+
+                  <td className="text-center px-3 align-middle border-l-0 border-r-0 text-m whitespace-nowrap p-3">
+                    <Link to={`/AttendanceUpdate/${data._id}`}>
+                      <button
+                        type="button"
+                        className="font-bold py-1 px-4 rounded-full mx-3 text-white"
+                        style={{ background: currentColor }}
+                      >
+                        <i className="fas fa-edit" />
+                      </button>
+                    </Link>
+                    <button
+                      type="button"
+                      className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-4 ml-2 rounded-full"
+                      onClick={() => {
+                        deleteAttendance(data._id);
+                      }}
+                    >
+                      <i className="fas fa-trash" />
+                    </button>
+                  </td>
+
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
 
