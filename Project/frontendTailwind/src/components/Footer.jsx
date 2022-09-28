692d509c8c2c5f793081d334b0fbc35675530ae6
Navod <54998118+navodveduth@users.noreply.github.com>
2022-09-28 13:50:31 +0530

    minor changes in footer

--- Project/frontendTailwind/src/components/Footer.jsx
+++ Project/frontendTailwind/src/components/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Footer = () => (
-  <div className="mt-24">
-    <p className="dark:text-gray-200 text-gray-700 text-center m-20 capitalize">
-      © 2022 All rights reserved by Y2_S2_WD_IT_02-WD_B02_ITP_14.
-    </p>
-  </div>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="mt-24">
+      <p className="dark:text-gray-200 text-gray-700 text-center m-20 capitalize">
+        © {currentYear} All rights reserved by Y2_S2_WD_IT_02-WD_B02_ITP_14.
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
