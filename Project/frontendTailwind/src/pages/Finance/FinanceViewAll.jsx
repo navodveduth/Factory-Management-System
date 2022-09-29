caf07b9ad10265df374271100422e30cf9ace750
it21003028 <it21003028@my.sliit.lk>
2022-09-29 12:35:55 +0530

    added search functionality to view - minod

--- Project/frontendTailwind/src/pages/Finance/FinanceViewAll.jsx
+++ Project/frontendTailwind/src/pages/Finance/FinanceViewAll.jsx
@@ -10,15 +10,14 @@ const FinanceViewAll = () => {
   const { currentColor } = useStateContext();
 
   const [transactions, setTransactions] = useState([]);
-  //const [recordedDate, setRecDate] = useState(' ');
+  const [searchTerm, setSearchTerm] = useState("");  //add this state to save filter word
 
   const getFinance = async () => {
     axios
       .get('http://localhost:8070/finance/viewTransaction')
       .then((res) => {
         setTransactions(res.data);
-       // const date = new Date(res.data.trnRecordedDate);
-       // setRecDate(date)
+
       })
       .catch((err) => {
         alert(err.message);
@@ -44,9 +43,16 @@ const FinanceViewAll = () => {
 
 
   return (
-    <div>
+
+    //add lines 49 to 53 to add the search bar to the page
+    //add lines 69 to 75 and adapt them to your view, or copy the entire return and change the variables
+
       <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg dark:text-white">
-        <Header category="Table" title="Employees" />
+        <div><div class="" style={{width:"70%", marginLeft:"85%", marginBottom:"-5%"}}>
+    <input type="search" class="form-control rounded" placeholder="Search Here" aria-label="Search" aria-describedby="search-addon" onChange={(e) => {
+    setSearchTerm(e.target.value);
+    }} /></div> 
+        <Header category="Table" title="Transactions" />
         <div className="block w-full overflow-x-auto rounded-lg">
           <table className="w-full rounded-lg">
             <thead>
@@ -60,8 +66,15 @@ const FinanceViewAll = () => {
               </tr>
             </thead>
             <tbody>
-              {transactions.map((data) => (
-                <tr className="text-sm h-10 border dark:border-slate-600">
+              {transactions.filter((data) => {
+                    if(searchTerm == ""){
+                        return data;
+                    }else if(data.trnID.toString().toLowerCase().includes(searchTerm.toLowerCase())){
+                        return data;
+                    }
+                }).map((data, key) => {
+                return(
+                <tr className="text-sm h-10 border dark:border-slate-600" key={key}>
                   <TableData value={data.trnID} />
                   <TableData value={data.trnDesc} />
                   <TableData value={data.trnAmount} />
@@ -89,7 +102,8 @@ const FinanceViewAll = () => {
                     </button>
                   </td>
                 </tr>
-              ))}
+                )
+              })}
             </tbody>
           </table>
         </div>
