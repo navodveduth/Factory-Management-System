1869fe47960554698bf159abb13848183a84b267
Navod <54998118+navodveduth@users.noreply.github.com>
2022-09-29 20:38:01 +0530

    minor changes in transport report

--- Project/frontendTailwind/src/pages/Transport/TransportReport.jsx
+++ Project/frontendTailwind/src/pages/Transport/TransportReport.jsx
@@ -24,7 +24,7 @@ const TransportReport = () => {
   }, []);
 
   const createPDF = () => {
-    const date = new Date(Date.now()).toISOString().split.T[0];
+    const date = new Date(Date.now()).toISOString().split('T')[0];
     const pdf = new jsPDF('landscape', 'px', 'a1', false);
     const data = document.querySelector('#tableContainer');
     pdf.html(data).then(() => {
@@ -44,7 +44,7 @@ const TransportReport = () => {
               type="button"
               className="py-1 px-4 rounded-lg text-white hover:bg-slate-700 bg-slate-500"
             >
-              Generate Report
+              Download Report
             </button>
           </div>
         </div>
