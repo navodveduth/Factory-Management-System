8c78fa64d6a31bd815f61370aab37c12ae759448
it21004322 <it21004322@my.sliit.lk>
2022-10-21 00:15:39 +0530

    added lookup

--- Project/backend/controllers/MachineryAndMaintenance/maintainence.controller.js
+++ Project/backend/controllers/MachineryAndMaintenance/maintainence.controller.js
@@ -4,7 +4,24 @@ import Maintainence from "../../models/MachineryAndMaintenance/maintainence.mode
 
 export const getAllMaintainenceDetails = async (req, res) => {
     try {
-        const maintainence = await Maintainence.find();
+        const maintainence = await Maintainence.aggregate([
+            {
+                $lookup: 
+                {
+                    from: "machinery",
+                    localField: "machineID" ,
+                    foreignField: "machineID",
+                    as: "machineDetails"
+                }
+
+                // $lookup:{
+                //     from: "driver",
+                //     localField: "vehicleNo" ,
+                //     foreignField: "vehicleNo",
+                //     as: "vehicleDetails"
+                // }
+            }
+        ]);
         res.status(200).json(maintainence);    
     } catch (error) {
         res.status(404).json({ message : error});
