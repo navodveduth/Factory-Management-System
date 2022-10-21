9ca8150f978b3597059d39a547c9be7fa026861d
it21004322 <it21004322@my.sliit.lk>
2022-10-21 20:08:56 +0530

    Small changes

--- Project/frontendTailwind/src/pages/MachineryAndMaintenance/MaintenanceViewAll.jsx
+++ Project/frontendTailwind/src/pages/MachineryAndMaintenance/MaintenanceViewAll.jsx
@@ -54,7 +54,7 @@ const MaintenanceViewAll = () => {
   return (
     <div>
       <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg dark:text-white">
-        <Header category="Table" title="Maintenance " />
+        <Header category="Table" title="Property Maintenance " />
 
         <div className=" flex items-center mb-5 ">
           <div>
@@ -78,12 +78,12 @@ const MaintenanceViewAll = () => {
             <thead>
               <tr className="bg-slate-200 text-md h-12 dark:bg-slate-800">
                 <TableHeader value="Type" />
-                <TableHeader value="Name" />
                 <TableHeader value="Service task & Shedule" />
                 <TableHeader value="Last Maintained Date" />
                 <TableHeader value="Next due" />
-                <TableHeader value="Total Cost" />
                 <TableHeader value="Status" />
+                <TableHeader value="Total Cost" />
+                <TableHeader value="Comments" />
                 <TableHeader value="Manage" />
               </tr>
             </thead>
@@ -102,11 +102,12 @@ const MaintenanceViewAll = () => {
                   }).map((data, key) => {
 
                     var datacolor = "text-black";
-                                if (data.status === "Completed") {
-                                    datacolor = "text-green-500 font-bold";
-                                } else {
-                                    datacolor = "text-red-600 font-bold";
-                                }
+                    if (data.status === "In progress") {
+                      datacolor = "text-red-600 font-bold";
+
+                    } else {
+                      datacolor = "text-green-500 font-bold";
+                    }
                   
                   return (
 
@@ -119,25 +120,17 @@ const MaintenanceViewAll = () => {
 
 
                   <TableData value={data.Type} />
-                  <TableData value={data.machineDetails.map((data3) => {
-                  
-                                return (
-                                  <div>
-                                    <TableData value = {data3.name} /> 
-                                  </div>
-                                )
-
-                              
-                
-                 })} />
                   <TableData value={data.Description} />
                   <TableData value={data.lastMaintainedDate.toString().split('T')[0]} />
                   <TableData value={data.nextServiceDate.toString().split('T')[0]} />
-                    <TableData value={data.others} />
+                  
+                    
                     <td className={`${datacolor} text-center px-3 align-middle border-l-0 border-r-0 text-m whitespace-nowrap p-3`}>{data.status} </td>
-                   
-
+                    <TableData value={data.others} />
+                    <TableData value={data.name} />
+                  
                   <td className="text-center px-3 align-middle border-l-0 border-r-0 text-m whitespace-nowrap p-3">
+                   
                   <Link to={`/MaintenanceUpdate/${data._id}`}>
                       <button
                         type="button"
