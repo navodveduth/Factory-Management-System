469bf55b4d552ba14a8c048c9756f3551d65b40f
it21004322 <it21004322@my.sliit.lk>
2022-10-21 20:13:45 +0530

    created a new model

---
+++ Project/backend/models/MachineryAndMaintenance/maintainenceMachine.model.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+
+const Schema = mongoose.Schema;
+
+const maintainenceMachineSchema = new Schema({
+
+    machineID:{
+        type: String,
+        required: true
+    },
+
+    name: {
+        type: String,
+        
+    },
+
+    Description : {
+        type : String,  
+        required : true
+    },
+   
+    lastMaintainedDate : {
+        type : Date,
+        required : true
+
+    },
+
+    nextServiceDate : {
+        type : Date,
+        required : true
+    },
+ 
+     status:{
+         type: String
+     },
+
+     Location:{
+            type: String
+        },
+
+    contactNo:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 10,
+        maxlength: 10,
+        pattern: "^[0-9]{10}$",
+    },
+
+    others:{
+        type: Number
+     }
+
+ 
+});
+
+const MaintainenceMachine= mongoose.model("maintainenceMachine" , maintainenceMachineSchema);
+
+export default MaintainenceMachine;
