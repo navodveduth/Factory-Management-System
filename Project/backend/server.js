fc79e753a6af8b62b5e1391b81f85fa8dcb6ffb4
it21004322 <it21004322@my.sliit.lk>
2022-10-21 20:14:49 +0530

    added new imports

--- Project/backend/server.js
+++ Project/backend/server.js
@@ -6,6 +6,8 @@ import bodyParser from 'body-parser';
 
 import machineryRouter from './routes/machinery.routes.js';
 import maintainenceRouter from './routes/maintainence.routes.js';
+import maintainenceMachineRouter from './routes/maintainenceMachine.routes.js';
+import maintainenceVehicleRouter from './routes/maintainenceVehicle.routes.js';
 import orderRouter from './routes/prod.routes.js';
 import financeRouter from './routes/finance.routes.js';
 import transportRouter from './routes/transport.routes.js';
@@ -29,6 +31,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/machinery', machineryRouter);
 app.use('/maintainence', maintainenceRouter);
+app.use('/maintainenceVehicle', maintainenceVehicleRouter);
+app.use('/maintainenceMachine', maintainenceMachineRouter);
 app.use('/production/order', orderRouter);
 app.use('/finance', financeRouter);
 app.use('/transport', transportRouter);
