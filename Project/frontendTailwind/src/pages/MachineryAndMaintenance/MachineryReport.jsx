9b9904c4f0f86b68be7f0a05e4cc1ff4e05a5bfa
it21004322 <it21004322@my.sliit.lk>
2022-11-04 23:23:00 +0530

    small changes

--- Project/frontendTailwind/src/pages/MachineryAndMaintenance/MachineryReport.jsx
+++ Project/frontendTailwind/src/pages/MachineryAndMaintenance/MachineryReport.jsx
@@ -4,6 +4,11 @@ import { jsPDF } from "jspdf";
 import { Header } from '../../components';
 import TableData from '../../components/Table/TableData';
 import TableHeader from '../../components/Table/TableHeader';
+import { useStateContext } from '../../contexts/ContextProvider';
+
+import { FiSettings } from 'react-icons/fi';
+import { Navbar, Footer, Sidebar, ThemeSettings } from '../../components';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 
 const MachineryReport = () => {
@@ -12,34 +17,93 @@ const MachineryReport = () => {
 
     var TotalDepreciation = 0;
     var TotalCost = 0;
+  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings, } = useStateContext();
 
-    const getMachinery = async () => {  //getMachinery is the function to get the data from the backend
-        axios.get("http://localhost:8070/machinery/")
-        .then((res) => { 
-            setMachinery(res.data); //setMachinery is used to update the state variable
-           
-    
-        })
-        .catch((err) => {
-            alert(err.message);
-        })
+  const getMachinery = async () => {  //getMachinery is the function to get the data from the backend
+    axios.get("http://localhost:8070/machinery/")
+    .then((res) => { 
+        setMachinery(res.data); //setMachinery is used to update the state variable
+       
+
+    })
+    .catch((err) => {
+        alert(err.message);
+    })
+}
+
+  useEffect(() => {
+    getMachinery(); // <== CHANGE ACCORDING TO YOUR OWN FUNCTIONS, YOU CAN REMOVE THIS LINE IF YOU DON'T NEED IT
+    const currentThemeColor = localStorage.getItem('colorMode'); // KEEP THESE LINES
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
     }
-    
-    useEffect(() => { //useEffect is used to call the function getMachinery
-        getMachinery();
-    }, [])
-    
-    const createPDF = () => {
-        const date = new Date(Date.now()).toISOString().split('T')[0];
-        const pdf = new jsPDF("landscape", "px", "a1",false);
-        const data = document.querySelector("#tableContainer");
-        pdf.html(data).then(() => {
-            pdf.save("MachineryReport-" + date+ ".pdf");
-           });
-    };
+  }, []);
 
-    return (
-        <div>
+  const createPDF = () => {
+    const date = new Date(Date.now()).toISOString().split('T')[0];
+    const pdf = new jsPDF("landscape", "px", "a1",false);
+    const data = document.querySelector("#tableContainer");
+    pdf.html(data).then(() => {
+        pdf.save("MachineryReport-" + date+ ".pdf");
+       });
+};
+
+
+  return (
+    <div>
+
+      {/* DON'T CHANGE ANYTHING HERE */}
+
+        <div className={currentMode === 'Dark' ? 'dark' : ''}>
+
+            <div className="flex relative dark:bg-main-dark-bg">
+
+                <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}> {/* THEME SETTINGS BUTTON */}
+                    <TooltipComponent content="Settings" position="Top">
+                    <button
+                        type="button"
+                        onClick={() => setThemeSettings(true)}
+                        style={{ background: currentColor, borderRadius: '50%' }}
+                        className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                    >
+                        <FiSettings />
+                    </button>
+                    </TooltipComponent>
+                </div>
+
+
+                {activeMenu ? ( // SIDEBAR IMPLEMENTATION
+                    <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+                    <Sidebar />
+                    </div>
+                ) : (
+                    <div className="w-0 dark:bg-secondary-dark-bg">
+                    <Sidebar />
+                    </div>
+                )}
+
+                <div
+                    className={ // MAIN BACKGROUND IMPLEMENTATION
+                    activeMenu
+                        ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                        : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                    }
+                >
+                    
+                    {/* NAVBAR IMPLEMENTATION */}
+                    <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+                        <Navbar />
+                    </div>
+
+                    <div>
+                        {themeSettings && <ThemeSettings />}
+                        <div>
+                            {/* YOUR COMPONENT IMPLEMENTATION GOES HERE */}
+                            {/* COPY YOUR ORIGINAL COMPONENT CODE HERE */}
+                            {/* PART AFTER THE RETURN STATEMENT */}
+                            <div>
             <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg dark:text-white">
 
                 <Header category="Report" title="Machinery" />
@@ -99,7 +163,14 @@ const MachineryReport = () => {
                 </div>
             </div>      
         </div>
-    );
-}
+                        </div>
+                        <Footer />
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </div>
+  );
+};
 
-export default MachineryReport
+export default MachineryReport;
