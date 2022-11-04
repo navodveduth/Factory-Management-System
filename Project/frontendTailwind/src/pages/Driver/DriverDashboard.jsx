cbe6f12891fac7978d06df8bff5eee0e8c78955d
Navod <54998118+navodveduth@users.noreply.github.com>
2022-11-04 17:30:31 +0530

    style: change driver dashboard

--- Project/frontendTailwind/src/pages/Driver/DriverDashboard.jsx
+++ Project/frontendTailwind/src/pages/Driver/DriverDashboard.jsx
@@ -1,14 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-import { FiUser } from 'react-icons/fi';
-import { DashTopBox, DashTopButton } from '../../components';
-import DriverViewAll from './DriverViewAll';
+import { FiUser, FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import {
+  DashTopBox,
+  DashTopButton,
+  Navbar,
+  Footer,
+  Sidebar,
+  ThemeSettings,
+} from '../../components';
 
 import { useStateContext } from '../../contexts/ContextProvider';
 
 const DriverDashboard = () => {
-  const { currentColor, currentMode } = useStateContext();
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
 
   const [driver, setDriver] = useState([]);
 
@@ -25,6 +40,12 @@ const DriverDashboard = () => {
 
   useEffect(() => {
     getDriver();
+    const currentThemeColor = localStorage.getItem('colorMode');
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
   }, []);
 
   const totDrivers = driver.length;
@@ -34,47 +55,93 @@ const DriverDashboard = () => {
   ).length;
 
   return (
-    <div className="mt-5">
-      <div className="flex flex-wrap lg:flex-nowrap justify-left ml-10 mt-5">
-        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-          {/* top buttons in the dashboard */}{' '}
-          {/* use for navigation buttons */}
-          <Link to="/DriverViewAll">
-            <DashTopButton value="View All Drivers Details" />
-          </Link>
-          <Link to="/DriverCreate">
-            <DashTopButton value="Create New Record" />
-          </Link>
-        </div>
-      </div>
+    <div>
+      <div className={currentMode === 'Dark' ? 'dark' : ''}>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            {' '}
+            {/* THEME SETTINGS BUTTON */}
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: '50%' }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
 
-      <div className="flex flex-wrap lg:flex-nowrap justify-center mt-5">
-        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-          {/* small top boxes in the dashboard */}
-          {/* use minimum 3, maximum 5 */}
-          <DashTopBox
-            icon={<FiUser />}
-            label="Total Drivers"
-            data={totDrivers}
-          />
-          <DashTopBox
-            icon={<FiUser />}
-            label="Available Drivers"
-            data={available}
-          />
-          <DashTopBox
-            icon={<FiUser />}
-            label="Unavailable Drivers"
-            data={unavailable}
-          />
+          {activeMenu ? ( // SIDEBAR IMPLEMENTATION
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+
+          <div
+            className={
+              // MAIN BACKGROUND IMPLEMENTATION
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }
+          >
+            {/* NAVBAR IMPLEMENTATION */}
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+
+            <div>
+              {themeSettings && <ThemeSettings />}
+              <div>
+                <div className="mt-5">
+                  <div className="flex flex-wrap lg:flex-nowrap justify-left ml-10 mt-5">
+                    <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+                      {/* top buttons in the dashboard */}
+                      {/* use for navigation buttons */}
+                      <Link to="/DriverViewAll">
+                        <DashTopButton value="View All Drivers Details" />
+                      </Link>
+                      <Link to="/DriverCreate">
+                        <DashTopButton value="New Driver" />
+                      </Link>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-wrap lg:flex-nowrap justify-center mt-5">
+                    <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+                      {/* small top boxes in the dashboard */}
+                      {/* use minimum 3, maximum 5 */}
+                      <DashTopBox
+                        icon={<FiUser />}
+                        label="Total Drivers"
+                        data={totDrivers}
+                      />
+                      <DashTopBox
+                        icon={<FiUser />}
+                        label="Available Drivers"
+                        data={available}
+                      />
+                      <DashTopBox
+                        icon={<FiUser />}
+                        label="Unavailable Drivers"
+                        data={unavailable}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <Footer />
+            </div>
+          </div>
         </div>
       </div>
     </div>
-
-    /* <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl  dark:bg-secondary-dark-bg dark:text-white ">
-        ----------use this div to create your graphs.--------------
-        ----------one div per graph -------------------------------
-       <div> */
   );
 };
 
