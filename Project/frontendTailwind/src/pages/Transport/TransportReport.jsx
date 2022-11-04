5f4e7f08d1a32ad4c56d8678346bd31031c8cb3a
Navod <54998118+navodveduth@users.noreply.github.com>
2022-11-04 16:49:15 +0530

    style: change transport report

--- Project/frontendTailwind/src/pages/Transport/TransportReport.jsx
+++ Project/frontendTailwind/src/pages/Transport/TransportReport.jsx
@@ -1,11 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { jsPDF } from 'jspdf';
-import { Header } from '../../components';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import {
+  Header,
+  Navbar,
+  Footer,
+  Sidebar,
+  ThemeSettings,
+} from '../../components';
 import TableData from '../../components/Table/TableData';
 import TableHeader from '../../components/Table/TableHeader';
 
+import { useStateContext } from '../../contexts/ContextProvider';
+
 const TransportReport = () => {
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
+
   const [transport, setTransport] = useState([]);
 
   const getTransport = async () => {
@@ -21,6 +41,12 @@ const TransportReport = () => {
 
   useEffect(() => {
     getTransport();
+    const currentThemeColor = localStorage.getItem('colorMode'); // KEEP THESE LINES
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
   }, []);
 
   const createPDF = () => {
@@ -34,59 +60,112 @@ const TransportReport = () => {
 
   return (
     <div>
-      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg dark:text-white">
-        <Header category="Report" title="Transport" />
-
-        <div className=" flex items-center mb-5 ">
-          <div className="mr-0 ml-auto">
-            <button
-              onClick={createPDF}
-              type="button"
-              className="py-1 px-4 rounded-lg text-white hover:bg-slate-700 bg-slate-500"
-            >
-              Download Report
-            </button>
+      <div className={currentMode === 'Dark' ? 'dark' : ''}>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
+            {' '}
+            {/* THEME SETTINGS BUTTON */}
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: '50%' }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
           </div>
-        </div>
 
-        <div
-          className="block w-full overflow-x-auto rounded-lg"
-          id="tableContainer"
-        >
-          <table className="w-full rounded-lg">
-            <thead>
-              <tr className="bg-slate-200 text-md h-12 dark:bg-slate-800">
-                <TableHeader value="Type" />
-                <TableHeader value="Destination Address" />
-                <TableHeader value="Date" />
-                <TableHeader value="Time" />
-                <TableHeader value="Distance" />
-                <TableHeader value="Transport Cost" />
-                <TableHeader value="Driver" />
-                <TableHeader value="Description" />
-                <TableHeader value="Status" />
-              </tr>
-            </thead>
+          {activeMenu ? ( // SIDEBAR IMPLEMENTATION
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
 
-            <tbody>
-              {transport.map((data, key) => (
-                <tr
-                  className="text-sm h-10 border dark:border-slate-600"
-                  key={key}
-                >
-                  <TableData value={data.type} />
-                  <TableData value={data.destinationAddress} />
-                  <TableData value={data.date.substring(0, 10)} />
-                  <TableData value={data.timeOfDispatch} />
-                  <TableData value={`${data.distance} km`} />
-                  <TableData value={`Rs. ${data.transportCost.toFixed(2)}`} />
-                  <TableData value={data.driver} />
-                  <TableData value={data.description} />
-                  <TableData value={data.status} />
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div
+            className={
+              // MAIN BACKGROUND IMPLEMENTATION
+              activeMenu
+                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+            }
+          >
+            {/* NAVBAR IMPLEMENTATION */}
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+              <Navbar />
+            </div>
+
+            <div>
+              {themeSettings && <ThemeSettings />}
+              <div>
+                <div>
+                  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg dark:text-white">
+                    <Header category="Report" title="Transport" />
+
+                    <div className=" flex items-center mb-5 ">
+                      <div className="mr-0 ml-auto">
+                        <button
+                          onClick={createPDF}
+                          type="button"
+                          className="py-1 px-4 rounded-lg text-white hover:bg-slate-700 bg-slate-500"
+                        >
+                          Download Report
+                        </button>
+                      </div>
+                    </div>
+
+                    <div
+                      className="block w-full overflow-x-auto rounded-lg"
+                      id="tableContainer"
+                    >
+                      <table className="w-full rounded-lg">
+                        <thead>
+                          <tr className="bg-slate-200 text-md h-12 dark:bg-slate-800">
+                            <TableHeader value="Type" />
+                            <TableHeader value="Destination Address" />
+                            <TableHeader value="Date" />
+                            <TableHeader value="Time" />
+                            <TableHeader value="Distance" />
+                            <TableHeader value="Transport Cost" />
+                            <TableHeader value="Driver" />
+                            <TableHeader value="Description" />
+                            <TableHeader value="Status" />
+                          </tr>
+                        </thead>
+
+                        <tbody>
+                          {transport.map((data, key) => (
+                            <tr
+                              className="text-sm h-10 border dark:border-slate-600"
+                              key={key}
+                            >
+                              <TableData value={data.type} />
+                              <TableData value={data.destinationAddress} />
+                              <TableData value={data.date.substring(0, 10)} />
+                              <TableData value={data.timeOfDispatch} />
+                              <TableData value={`${data.distance} km`} />
+                              <TableData
+                                value={`Rs. ${data.transportCost.toFixed(2)}`}
+                              />
+                              <TableData value={data.driver} />
+                              <TableData value={data.description} />
+                              <TableData value={data.status} />
+                            </tr>
+                          ))}
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <Footer />
+            </div>
+          </div>
         </div>
       </div>
     </div>
