bf94828a732ddd989faf05e41fa71e198fa5ad22
it21003028 <it21003028@my.sliit.lk>
2022-11-06 02:10:06 +0530

    finance frontend files

--- Project/frontendTailwind/src/pages/Finance/FinanceDashboard.jsx
+++ Project/frontendTailwind/src/pages/Finance/FinanceDashboard.jsx
@@ -7,9 +7,14 @@ import { DashTopBox, DashTopButton, TransactionPieChart } from '../../components
 import { useStateContext } from '../../contexts/ContextProvider';
 import { ParetoSeries } from '@syncfusion/ej2-react-charts';
 
+import { FiUser } from 'react-icons/fi';
+import { FiSettings } from 'react-icons/fi';
+import { Navbar, Footer, Sidebar, ThemeSettings } from '../../components';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 
 const FinanceDashboard = () => {
-  const { currentColor, currentMode } = useStateContext();
+  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings, } = useStateContext();
 
   const [TRN, setTransactions] = useState([]);
 
@@ -26,46 +31,110 @@ const FinanceDashboard = () => {
 
   useEffect(() => {
     getFinance();
+    const currentThemeColor = localStorage.getItem('colorMode'); // KEEP THESE LINES
+    const currentThemeMode = localStorage.getItem('themeMode');
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
   }, []);
 
   var totalRev = 0, totalExp = 0, exp2022 =0;
 
   return (
+    <div>
 
-    <div className="mt-5">
+      {/* DON'T CHANGE ANYTHING HERE */}
 
-      {TRN.filter((TRN) => TRN.trnType === 'Revenue').map((TRN) => {
-        totalRev += TRN.trnAmount;
-        })}
-      {TRN.filter((TRN) => TRN.trnType === 'Expense').map((TRN) => {
-        totalExp += TRN.trnAmount;
-        })}
+        <div className={currentMode === 'Dark' ? 'dark' : ''}>
 
-      <div className="flex flex-wrap lg:flex-nowrap justify-left ml-10 mt-5">
-        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-          {/* top buttons in the dashboard */} {/* use for navigation buttons*/}
-          <Link to="/FinanceViewAll">
-            <DashTopButton value="View All Transactions" />
-          </Link>
-          <Link to="/FinanceNew">
-            <DashTopButton value="Cash Transactions" />
-          </Link>
+            <div className="flex relative dark:bg-main-dark-bg">
+
+                <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}> {/* THEME SETTINGS BUTTON */}
+                    <TooltipComponent content="Settings" position="Top">
+                    <button
+                        type="button"
+                        onClick={() => setThemeSettings(true)}
+                        style={{ background: currentColor, borderRadius: '50%' }}
+                        className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+                    >
+                        <FiSettings />
+                    </button>
+                    </TooltipComponent>
+                </div>
+
+
+                {activeMenu ? ( // SIDEBAR IMPLEMENTATION
+                    <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+                    <Sidebar />
+                    </div>
+                ) : (
+                    <div className="w-0 dark:bg-secondary-dark-bg">
+                    <Sidebar />
+                    </div>
+                )}
+
+                <div
+                    className={ // MAIN BACKGROUND IMPLEMENTATION
+                    activeMenu
+                        ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
+                        : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                    }
+                >
+                    
+                    {/* NAVBAR IMPLEMENTATION */}
+                    <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
+                        <Navbar />
+                    </div>
+
+                    <div>
+                        {themeSettings && <ThemeSettings />}
+                        <div>
+                            {/* start*/}
+
+                            <div className="mt-5">
+
+                            {TRN.filter((TRN) => TRN.trnType === 'Revenue').map((TRN) => {
+                              totalRev += TRN.trnAmount;
+                              })}
+                            {TRN.filter((TRN) => TRN.trnType === 'Expense').map((TRN) => {
+                              totalExp += TRN.trnAmount;
+                              })}
+
+                            <div className="flex flex-wrap lg:flex-nowrap justify-left ml-10 mt-5">
+                              <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+                                {/* top buttons in the dashboard */} {/* use for navigation buttons*/}
+                                <Link to="/FinanceViewAll">
+                                  <DashTopButton value="View All Transactions" />
+                                </Link>
+                                <Link to="/FinanceNew">
+                                  <DashTopButton value="Cash Transactions" />
+                                </Link>
+                              </div>
+                            </div>
+
+                            <div className="flex flex-wrap lg:flex-nowrap justify-center mt-5">
+                              <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+                                {/* small top boxes in the dashboard */} {/* use minimum 3, maximum 5 */}
+                                <DashTopBox icon={<RiMoneyDollarCircleFill />} label="Revenue Total" data= {"Rs."+totalRev} />
+                                <DashTopBox icon={<RiMoneyDollarCircleFill />} label="Expenses Total" data={"Rs."+totalExp} />
+                              </div>
+                            </div>
+
+                            <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl  dark:bg-secondary-dark-bg dark:text-white ">
+                                <TransactionPieChart />
+                            </div>
+                            </div>
+
+                            {/* end */}
+                            
+                        </div>
+                        <Footer />
+                    </div>  
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div className="flex flex-wrap lg:flex-nowrap justify-center mt-5">
-        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-          {/* small top boxes in the dashboard */} {/* use minimum 3, maximum 5 */}
-          <DashTopBox icon={<RiMoneyDollarCircleFill />} label="Revenue Total" data= {"Rs."+totalRev} />
-          <DashTopBox icon={<RiMoneyDollarCircleFill />} label="Expenses Total" data={"Rs."+totalExp} />
-        </div>
-      </div>
-
-      <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl  dark:bg-secondary-dark-bg dark:text-white ">
-          <TransactionPieChart />
-      </div>
     </div>
-
     /* <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl  dark:bg-secondary-dark-bg dark:text-white ">
         ----------use this div to create your graphs.--------------
         ----------one div per graph -------------------------------
