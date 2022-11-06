2a25bcf1c8d45acdd386cbc5a5ca912e1af34289
Navod <54998118+navodveduth@users.noreply.github.com>
2022-11-07 00:18:56 +0530

    export TransportDateRange

--- Project/frontendTailwind/src/pages/Transport/index.jsx
+++ Project/frontendTailwind/src/pages/Transport/index.jsx
@@ -2,5 +2,5 @@ export { default as TransportDashboard } from './TransportDashboard';
 export { default as TransportNew } from './TransportNew';
 export { default as TransportUpdate } from './TransportUpdate';
 export { default as TransportViewAll } from './TransportViewAll';
+export { default as TransportDateRange } from './TransportDateRange';
 export { default as TransportReport } from './TransportReport';
-
