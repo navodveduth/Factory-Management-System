23e07a1373ebdff3f2ca58817386e3a83f6294e6
Navod <54998118+navodveduth@users.noreply.github.com>
2022-11-07 13:38:22 +0530

    export TransportRecord

--- Project/frontendTailwind/src/pages/Transport/index.jsx
+++ Project/frontendTailwind/src/pages/Transport/index.jsx
@@ -3,4 +3,5 @@ export { default as TransportNew } from './TransportNew';
 export { default as TransportUpdate } from './TransportUpdate';
 export { default as TransportViewAll } from './TransportViewAll';
 export { default as TransportDateRange } from './TransportDateRange';
+export { default as TransportRecord } from './TransportRecord';
 export { default as TransportReport } from './TransportReport';
