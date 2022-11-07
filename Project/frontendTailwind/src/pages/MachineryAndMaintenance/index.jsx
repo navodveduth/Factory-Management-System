f51c26babb0b08c905a93241374f7d9b70921565
it21004322 <it21004322@my.sliit.lk>
2022-11-07 11:52:03 +0530

    added date date sort imports

--- Project/frontendTailwind/src/pages/MachineryAndMaintenance/index.jsx
+++ Project/frontendTailwind/src/pages/MachineryAndMaintenance/index.jsx
@@ -2,26 +2,32 @@ export { default as MachineryViewAll } from './MachineryViewAll';
 export { default as MachineryNew } from './MachineryNew';
 export { default as MachineryUpdate } from './MachineryUpdate';
 export { default as MachineryDashboard } from  './MachineryDashboard';
+export {default as MachineryDateRange} from './MachineryDateRange';
+export {default as MachineryReport} from './MachineryReport';
+export {default as MachMaintenanceHistory} from './MachMaintenanceHistory';
 
 export {default as MaintenanceDashboard}  from './MaintenanceDashboard';
 export {default as MaintenanceViewAll}  from './MaintenanceViewAll';
 export {default as MaintenanceNew}  from './MaintenanceNew';
 export {default as MaintenanceUpdate}  from './MaintenanceUpdate';
+export {default as MaintainenceTask}  from './MaintainenceTask';
+export {default as MaintainenceReport} from './MaintainenceReport';
+export {default as MaintainenceDateRange} from './MaintainenceDateRange';
+
 
 export {default as MachMaintenanceViewAll}  from './MachMaintenanceViewAll';
 export {default as MachMaintenanceNew}  from './MachMaintenanceNew';
 export {default as MachMaintenanceUpdate}  from './MachMaintenanceUpdate';
 export {default as MachMaintenanceReport}  from './MachMaintenanceReport';
+export {default as MachManitenanceDateRange}  from './MachManitenanceDateRange';
 
 export {default as VehiMaintenanceViewAll}  from './VehiMaintenanceViewAll';
 export {default as VehiMaintenanceNew}  from './VehiMaintenanceNew';
 export {default as VehiMaintenanceUpdate}  from './VehiMaintenanceUpdate';
 export {default as VehiMaintenanceReport}  from './VehiMaintenanceReport';
+export {default as VehiMaintenanceDateRange}  from './VehiMaintenanceDateRange';
 
-export {default as MaintainenceTask}  from './MaintainenceTask';
-export {default as MaintainenceReport} from './MaintainenceReport';
-export {default as MachineryReport} from './MachineryReport';
-export {default as MachMaintenanceHistory} from './MachMaintenanceHistory';
+
 
 
 
