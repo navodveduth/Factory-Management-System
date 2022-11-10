ea47a9bdeb20cbf93a151dca59c5801f903f8fb8
Minod Perera <87423225+IT21003028@users.noreply.github.com>
2022-11-11 03:22:16 +0530

    added confirm popup finance

--- Project/frontendTailwind/src/pages/Finance/IncomeStatementDateRangePreview.jsx
+++ Project/frontendTailwind/src/pages/Finance/IncomeStatementDateRangePreview.jsx
@@ -10,6 +10,7 @@ import { FiSettings } from 'react-icons/fi';
 import { Navbar, Footer, Sidebar, ThemeSettings } from '../../components';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import logo from '../../data/logo.png';
+import Swal from 'sweetalert2';
 
 
 const IncomeStatementPreview = () => {
@@ -35,6 +36,20 @@ const IncomeStatementPreview = () => {
     date.setFullYear(date.getFullYear() - years);
     return date;
   }
+
+  const downloadConf = ()=>{
+    Swal.fire({
+      title: 'Downloading!',
+      text: "Your download has begun!",
+      icon: 'success',
+      showCancelButton: false,
+      color: '#f8f9fa',
+      background: '#6c757d',
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'OK!'
+    })
+  };
 
   var curDate = new Date(location.state.DS);   
   var curDate_minus1 = new Date(location.state.DE);
@@ -358,7 +373,7 @@ const IncomeStatementPreview = () => {
                           <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-secondary-dark-bg dark:text-white">
                             <div className=" flex items-center mb-5 ">
                                 <div className="mr-0 ml-auto">
-                                    <button onClick={createPDF} type="button" className="py-1 px-4 rounded-lg text-white hover:bg-slate-700 bg-slate-500" >Download</button>
+                                    <button onClick={()=>{createPDF(); downloadConf();}} type="button" className="py-1 px-4 rounded-lg text-white hover:bg-slate-700 bg-slate-500" >Download</button>
                                 </div>
                             </div>
 
